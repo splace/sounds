c899a215b9133c921ef5894089a40862f8569dde
simon <psiplace@netscape.net>
2016-06-03 20:19:36 +0100

    comment

--- modifiers.go
+++ modifiers.go
@@ -1,30 +1,41 @@
 package sound
 
 import (
-	"github.com/splace/signals" //"../signals"//
+	"github.com/splace/signals" 
 	"time"
 )
 
+//  returns a Sound that is another Sound with silence prepended.   
 func Delayed(source Sound, offset time.Duration) Sound {
 	return NewSound(signals.Shifted{signals.Modulated{signals.Heavyside{}, source}, signals.X(offset.Seconds())}, offset+time.Duration(source.MaxX()))
 }
 
+// returns a Sound that is another Sound rate adjusted.
 func Spedup(source Sound, factor float32) Sound {
 	return NewSound(signals.Spedup{source, factor}, time.Duration(float32(source.MaxX())/factor))
 }
 
+// returns a Delayed Sound, so it starts just as another sounds ends.
 func After(waitedFor, source Sound) Sound {
 	return Delayed(source, time.Duration(waitedFor.MaxX()))
 }
 
+// returns a Delayed Sound, so it starts from an offset after another sounds ends.
 func AfterPlusOffset(waitedFor, source Sound, offset time.Duration) Sound {
 	return Delayed(source, time.Duration(waitedFor.MaxX())+offset)
 }
 
+// returns a Delayed Sound, so it starts from an offset before another sounds ends.
+func AfterMinusOffset(waitedFor, source Sound, offset time.Duration) Sound {
+	return Delayed(source, time.Duration(waitedFor.MaxX())-offset)
+}
+
+// returns a sound in reversed.
 func Reversed(source Sound) Sound {
 	return NewSound(signals.Shifted{signals.Reversed{source}, source.MaxX()}, time.Duration(source.MaxX()))
 }
 
+// returns a sound that is rate adjusted depending on the value of a signal, potentially another sound. 
 func Modulated(source Sound, modulation signals.Signal, factor time.Duration) Sound {
 	return NewSound(signals.RateModulated{source, modulation, signals.X(factor.Seconds())}, time.Duration(source.MaxX()))
 }
