0cd9a05aadba9393a85535df27ff914d23a0cdef
simon <psiplace@netscape.net>
2016-07-31 22:02:44 +0100

    stdout pipe output if no name

--- examples/DTMFmaker/DTMFmaker.go
+++ examples/DTMFmaker/DTMFmaker.go
@@ -4,6 +4,7 @@ package main
 import . "github.com/splace/sounds"
 import "github.com/splace/sounds/DTMF"
 import "os"
+import "io"
 import "flag"
 import "time"
 import "log"
@@ -15,27 +16,24 @@ func main() {
 	flag.DurationVar(&gap, "gap", 50*time.Millisecond, "gap between pulses")
 	flag.DurationVar(&width, "width", 80*time.Millisecond, "pulse width")
 	var sampleRate, sampleBytes uint
-	flag.UintVar(&sampleRate, "rate", 22050, "sample per second")
+	flag.UintVar(&sampleRate, "rate", 8000, "sample per second")
 	flag.UintVar(&sampleBytes, "bytes", 2, "bytes per sample")
 	codeString := flag.String("code", "0123456789", "<digits>*#ABCD to be encoded.")
 
 	flag.Parse()
 	file := flag.Args()
 	code := []rune(*codeString)
-	var wavFile *os.File
+	var stream io.WriteCloser
 	var err error
 	if len(file) > 0 {
-		wavFile, err = os.Create(file[0])
+		stream, err = os.Create(file[0])
 		if err != nil {
 			log.Fatal(file[0], "Creation failure", err.Error())
 		}
-		defer wavFile.Close()
 	} else {
-		log.Fatal("need an output file name argument.")
+		stream = os.Stdout
 	}
-	if len(code) == 0 {
-		log.Fatal("need a least 1 key.")
-	}
+	defer stream.Close()
 	// the minimum inter-digit interval shall be  45msec, the minimum pulse duration shall be 50msec, and the minimum duty cycle for ANSI-compliance shall be 100msec
 	if gap<45*time.Millisecond || width<50*time.Millisecond || gap+width<100*time.Millisecond {
 		log.Println("Warning: Non-ANSI-complient timings.")
@@ -45,7 +43,7 @@ func main() {
 	for i, c := range code {
 		tones[i] = NewNote(DTMF.Tones[c], width)
 	}
-	Encode(wavFile, int(sampleBytes), int(sampleRate), NewSequencer(SoundsSeparated(Silence(gap),tones...)...))
+	Encode(stream, int(sampleBytes), int(sampleRate), NewSequencer(SoundsSeparated(Silence(gap),tones...)...))
 }
 
 
